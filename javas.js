d81c52df4f3b56c743889c4509aa66193da22caf
KotyaraC <149381770+KotyaraC@users.noreply.github.com>
2024-04-22 10:43:23 +0300

    Update javas.js

--- javas.js
+++ javas.js
@@ -1,23 +1,33 @@
+let lastTouchTime = 0;
+
+document.addEventListener('touchstart', function(event) {
+    let currentTime = new Date().getTime();
+    if (currentTime - lastTouchTime < 300) {
+        event.preventDefault();
+        event.stopPropagation();
+        alert('Створення скріншотів заборонено на цьому сайті.');
+    }
+    lastTouchTime = currentTime;
+}, false);
+
 document.addEventListener('keydown', function(e) {
     if (e.key === 'PrintScreen' || (e.ctrlKey && (e.keyCode === 67 || e.keyCode === 86))) {
         e.preventDefault();
-        alert('Скриншоти заборонені на цьому сайті.');
+        e.stopPropagation();
+        alert('Скріншоти заборонені на цьому сайті.');
     }
 });
 
 document.addEventListener('keyup', function(e) {
     if (e.key === 'PrintScreen' || (e.ctrlKey && (e.keyCode === 67 || e.keyCode === 86))) {
         e.preventDefault();
-        alert('Скриншоти заборонені на цьому сайті.');
+        e.stopPropagation();
+        alert('Скріншоти заборонені на цьому сайті.');
     }
 });
 
 window.addEventListener('contextmenu', function(e) {
     e.preventDefault();
+    e.stopPropagation();
     alert('Контекстне меню заборонене на цьому сайті.');
-});
-
-document.addEventListener('touchstart', function(e) {
-    e.preventDefault();
-    alert('Створення скріншотів заборонено на цьому сайті.');
 });
